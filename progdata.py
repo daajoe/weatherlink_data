d9963100a3821c8000f7ea93bbfc6d495f585053
Russell Parker <russell.parker7@gmail.com>
2016-01-15 13:43:31 -0500

    Store program data in separate file

---
+++ progdata.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# A list of all the data fields. In the setup, any fields that the client
+# wants to exclude will be turned to empty strings. 
+ordFieldsA = ["date", "time", "out_temp", "hi_out_temp", "low_out_temp", 
+            "rainfall", "hi_rain_rate", "barometer", "solar_rad", 
+            "num_wind_samples", "inside_temp", "in_humidity", "out_humidity",
+            "avg_wind", "hi_wind", "hi_wind_dir", "prevailing_dir", "avg_UV",
+            "ET", "high_solar_rad", "high_UV", "forecast_rule", "leaf_temp1",
+            "leaf_temp2", "leaf_wet1", "leaf_wet2", "soil_temp1", "soil_temp2",
+            "soil_temp3", "soil_temp4", "extra_hum1", "exta_hum2", 
+            "extra_temp1", "extra_temp2", "extra_temp3", "soil_moist1", 
+            "soil_moist2", "soil_moist3", "soil_moist4"
+]
